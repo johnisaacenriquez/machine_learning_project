2480165b40e501d31c853e6df65beb780a653ae3
johnisaacenriquez <johnisaacenriquez@cp.prec.eng.osaka-u.ac.jp>
2024-08-19 13:13:43 +0900

    Logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -23,5 +23,5 @@ if __name__=="__main__":
         a = 1 / 0
     
     except Exception as e:
-        raise CustomException(e, sys)
         logging.info("Division by zero.")
+        raise CustomException(e, sys)
