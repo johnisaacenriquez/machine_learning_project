62315eabe8e8e1ef4b878e0ce5fb48cc17a57ca0
johnisaacenriquez <johnisaacenriquez@cp.prec.eng.osaka-u.ac.jp>
2024-08-21 07:53:11 +0900

    Model training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -60,7 +60,6 @@ class DataTransformation:
 
             preprocessing_obj = self.get_data_transformer_object()
             target_column_name = 'math_score'
-            numerical_columns = ['reading_score', 'writing_score']
             
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
